6f4ee8354c1bfa2d1317171e430f6c391052732e
eric <785340726@qq.com>
2019-03-16 13:28:47 +0800

    debug

--- data/dataset.py
+++ data/dataset.py
@@ -104,12 +104,12 @@ class Dataset:
         self.tsf = Transform(opt.min_size, opt.max_size)
 
     def __getitem__(self, idx):
-        ori_img, bbox, label, difficult = self.db.get_example(idx)
+        ori_img, bbox, label, difficult, curb_class_label = self.db.get_example(idx)
 
         img, bbox, label, scale = self.tsf((ori_img, bbox, label))
         # TODO: check whose stride is negative to fix this instead copy all
         # some of the strides of a given numpy array are negative.
-        return img.copy(), bbox.copy(), label.copy(), scale
+        return img.copy(), bbox.copy(), label.copy(), scale, curb_class_label
 
     def __len__(self):
         return len(self.db)
@@ -121,9 +121,9 @@ class TestDataset:
         self.db = CurbROIDataset(opt.voc_data_dir, split=split, use_difficult=use_difficult)
 
     def __getitem__(self, idx):
-        ori_img, bbox, label, difficult = self.db.get_example(idx)
+        ori_img, bbox, label, difficult,curb_class_label = self.db.get_example(idx)
         img = preprocess(ori_img)
-        return img, ori_img.shape[1:], bbox, label, difficult
+        return img, ori_img.shape[1:], bbox, label, difficult, curb_class_label
 
     def __len__(self):
         return len(self.db)
@@ -134,9 +134,8 @@ class PredictDataset:
         self.db = CurbROIDataset(opt.voc_data_dir, split=split, use_difficult=use_difficult)
 
     def __getitem__(self, idx):
-        ori_img, bbox, label, difficult = self.db.get_example(idx)
+        ori_img, bbox, label, difficult, curb_class_label = self.db.get_example(idx)
         img = preprocess(ori_img)
-        return img, ori_img.shape[1:], bbox, label, difficult
-
+        return img, ori_img.shape[1:], bbox, label, difficult, curb_class_label
     def __len__(self):
         return len(self.db)
--- train.py
+++ train.py
@@ -73,7 +73,7 @@ def predictor(dataloader, faster_rcnn, test_num=10000):
     json_file = open(json_path,'w')
     for i in range(len(pred_bboxes)):
         maxindex = pred_scores[i].argmax()
-        jlist.append({str(i+1):pred_bboxes[i][maxindex].tolist()})
+        jlist.append({str(i+1).zerofill(6):pred_bboxes[i][maxindex].tolist()})
     json.dump(jlist,json_file,indent=1)
     json_file.close()
     print('predict %d images successfully, the result is saved in %s'%(test_num,json_file))
