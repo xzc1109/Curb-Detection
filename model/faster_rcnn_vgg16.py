8cc919a2cd6bf8b38b689974b0c42499638428ce
xzc1109 <785340726@qq.com>
2019-03-30 21:22:01 +0800

    stable

--- model/faster_rcnn_vgg16.py
+++ model/faster_rcnn_vgg16.py
@@ -122,7 +122,6 @@ class SceneClassifier(nn.Module):
     def forward(self, x):
         #x = self.conv(x)
         x = self.maxpool(x)
-        print(x.shape)
         x = x.view(x.size(0), -1)
         x = self.classifier(x)
         x = self.score(x)
